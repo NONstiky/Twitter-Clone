adee3c46230f175a917ce564b4d72ab16c3e30e8
Marcos Banchik <mbanchik@fb.com>
2017-06-26 13:57:56 -0700

    finished episode 7

---
+++ android-rest-client-template-master /app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,30 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by mbanchik on 6/26/17.
+ */
+
+public class Tweet {
+
+    // list out the attributes
+    public String body;
+    public long uid; // database ID for the tweet
+    public User user;
+    public String createdAt;
+
+    // deserialize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException{
+        Tweet tweet = new Tweet();
+
+        // extract the values from JSON
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+}
+
