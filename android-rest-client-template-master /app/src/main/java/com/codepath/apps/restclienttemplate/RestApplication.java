a6b881f95cca437e394666f837a8ce1f706a29de
Marcos Banchik <mbanchik@fb.com>
2017-06-26 11:24:56 -0700

    initialized folder

---
+++ android-rest-client-template-master /app/src/main/java/com/codepath/apps/restclienttemplate/RestApplication.java
@@ -0,0 +1,35 @@
+package com.codepath.apps.restclienttemplate;
+
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowLog;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+import android.app.Application;
+import android.content.Context;
+
+/*
+ * This is the Android application itself and is used to configure various settings
+ * including the image cache in memory and on disk. This also adds a singleton
+ * for accessing the relevant rest client.
+ *
+ *     RestClient client = RestApplication.getRestClient();
+ *     // use client to send requests to API
+ *
+ */
+public class RestApplication extends Application {
+	private static Context context;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		FlowManager.init(new FlowConfig.Builder(this).build());
+		FlowLog.setMinimumLoggingLevel(FlowLog.Level.V);
+
+		RestApplication.context = this;
+	}
+
+	public static RestClient getRestClient() {
+		return (RestClient) RestClient.getInstance(RestClient.class, RestApplication.context);
+	}
+}
